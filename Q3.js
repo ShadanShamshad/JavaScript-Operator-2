6765bfbc99467d8f3220fc45419efc1cf98ca839
Shadan Shamshad <shadanshamshad@gmail.com>
2023-12-09 21:05:22 +0530

    Javascript Operator 2 Assignment

---
+++ Q3.js
@@ -0,0 +1,20 @@
+        // Use a nested ternary operator to check that a number is positive, negative or zero. You have to print “positive” if the number is positive and similarly for negative and zero also.
+
+
+        
+        let num = -10;
+
+        let result = num > 0 
+
+        ? "Positive" 
+
+        : num < 0 
+
+        ? "Negative" 
+
+        : "Zero" ;
+
+        console.log(result);
+
+        //  OUTPUT
+        //  Negative
---
+++ Q4.js
@@ -0,0 +1,10 @@
+            // Use a ternary operator to check that a person is eligible to vote or not by checking his age. If the age of the person is less than 18 then “You cannot vote” should be logged else “You can vote” should be logged.
+
+
+                const age = 18; 
+                const canVote = age >= 18 ? 'Yes, you can vote' : 'No, you cannot vote' ;  
+                console.log(`Can You vote ? ${canVote}`);
+
+            // Output : Can You vote ? Yes, you can vote
+  
+   
